1a9a59740264b13779f58189d752cbc3570e67a9
amartinez <adriana.martinez.dev@gmail.com>
2026-07-22 19:19:44 -0600

    Create Agent logic for conversation

--- Main.py
+++ Main.py
@@ -9,9 +9,22 @@ from openai import OpenAI
 
 client = OpenAI()
 
-chat_completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello world!"}]
-)
+class Agent:
+    def __init__(self, system=""):
+        self.system = system
+        self.messages = []
+        if self.system:
+            self.messages.append({"role": "system", "content": system})
 
-print(chat_completion.choices[0].message.content)
+    def __call__(self, message):
+        self.messages.append({"role": "user", "content": message})
+        result = self.excecute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+
+    def excecute(self):
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            temperature=0,
+            messages=self.messages)
+        return completion.choices[0].message.content
