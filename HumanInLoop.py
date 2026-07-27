ab0f0131baf094a13e60e642a3c254332a98879c
amartinez <adriana.martinez.dev@gmail.com>
2026-07-27 10:39:50 -0600

    Continue after interrupt

--- HumanInLoop.py
+++ HumanInLoop.py
@@ -102,3 +102,24 @@ for event in abot.graph.stream({"messages": messages}, thread):
 
 abot.graph.get_state(thread)
 abot.graph.get_state(thread).next
+
+for event in abot.graph.stream(None, thread):
+    for v in event.values():
+        print(v)
+abot.graph.get_state(thread)
+abot.graph.get_state(thread).next
+
+messages = [HumanMessage("Whats the weather in LA?")]
+thread = {"configurable": {"thread_id": "2"}}
+for event in abot.graph.stream({"messages": messages}, thread):
+    for v in event.values():
+        print(v)
+while abot.graph.get_state(thread).next:
+    print("\n", abot.graph.get_state(thread),"\n")
+    _input = input("proceed?")
+    if _input != "y":
+        print("aborting")
+        break
+    for event in abot.graph.stream(None, thread):
+        for v in event.values():
+            print(v)
