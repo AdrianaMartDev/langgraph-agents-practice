2f8937c0df412305825e42b375d23956dce6da08
amartinez <adriana.martinez.dev@gmail.com>
2026-07-27 10:38:33 -0600

    Manual human approval

--- HumanInLoop.py
+++ HumanInLoop.py
@@ -47,3 +47,58 @@ class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], reduce_messages]
 
 tool = TavilySearchResults(max_results=2)
+
+class Agent:
+    def __init__(self, model, tools, system="", checkpointer=None):
+        self.system = system
+        graph = StateGraph(AgentState)
+        graph.add_node("llm", self.call_openai)
+        graph.add_node("action", self.take_action)
+        graph.add_conditional_edges("llm", self.exists_action, {True: "action", False: END})
+        graph.add_edge("action", "llm")
+        graph.set_entry_point("llm")
+        self.graph = graph.compile(
+            checkpointer=checkpointer,
+            interrupt_before=["action"]
+        )
+        self.tools = {t.name: t for t in tools}
+        self.model = model.bind_tools(tools)
+
+    def call_openai(self, state: AgentState):
+        messages = state['messages']
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        return {'messages': [message]}
+
+    def exists_action(self, state: AgentState):
+        print(state)
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+
+    def take_action(self, state: AgentState):
+        tool_calls = state['messages'][-1].tool_calls
+        results = []
+        for t in tool_calls:
+            print(f"Calling: {t}")
+            result = self.tools[t['name']].invoke(t['args'])
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+        print("Back to the model!")
+        return {'messages': results}
+
+prompt = """You are a smart research assistant. Use the search engine to look up information. \
+You are allowed to make multiple calls (either together or in sequence). \
+Only look up information when you are sure of what you want. \
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+"""
+model = ChatOpenAI(model="gpt-3.5-turbo")
+abot = Agent(model, [tool], system=prompt, checkpointer=memory)
+
+messages = [HumanMessage(content="Whats the weather in SF?")]
+thread = {"configurable": {"thread_id": "1"}}
+for event in abot.graph.stream({"messages": messages}, thread):
+    for v in event.values():
+        print(v)
+
+abot.graph.get_state(thread)
+abot.graph.get_state(thread).next
